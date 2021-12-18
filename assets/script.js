f5efa1c9b0c45c94a465588b4f4512625402d6f5
Timrob584 <timrob584@gmail.com>
2021-12-18 10:21:14 -0500

    added results page

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var city = [];
 var searchInput = document.getElementById("cityNameInput");
-
+var googleKey = "AIzaSyCQm1aQeB2zDpJD1owojwGE6O5P2IE9TyY";
 
 // Open Brewery API Call
 function getBreweries(city) {
